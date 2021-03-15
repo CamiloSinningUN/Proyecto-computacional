8baf4b8627fae2211a43901b4e8f6fd58ccff1d7
CamiloSinningUN <cjsinning@uninorte.edu.co>
2021-03-15 16:04:03 -0500

    sin_ceros

--- main.py
+++ main.py
@@ -16,8 +16,29 @@ def sin_repetidos(a, b):
     tupla = (cont,lista)
     return tupla
 
-switch = int(input("Digite el numero correspondiente al metodo que desea usar:\n + sin_repetidos (1) \n >"))
+def sin_ceros(a, b):
+    lista = []
+    cont = 0
+    for i in range(a,b+1):   
+        sw = True
+        i = str(i)
+        for j in i:
+            if("0" in i):
+                sw = False
+                continue
+        if sw:
+            lista.append(i)
+            cont+=1
+    tupla = (cont,lista)
+    return tupla
+
+
+switch = int(input("Digite el numero correspondiente al metodo que desea usar:\n + sin_repetidos (1) \n + sin_ceros (2)\n >"))
 if switch == 1:
     a = int(input("Ingrese a: "))
     b = int(input("Ingrese b: "))
     print("El resultado es: ",sin_repetidos(a,b))
+elif switch == 2:
+    a = int(input("Ingrese a: "))
+    b = int(input("Ingrese b: "))
+    print("El resultado es: ",sin_ceros(a,b))
